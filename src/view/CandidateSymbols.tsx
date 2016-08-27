c2894cfeb5b7d4e5a7782296f944ce046624891c
Ting-Yan Lai <banacorn@gmail.com>
2016-08-28 00:37:46 +0800

    <CandidateSymbols> frame

--- src/view/CandidateSymbols.tsx
+++ src/view/CandidateSymbols.tsx
@@ -3,12 +3,17 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import * as classNames from 'classnames';
 import { View } from '../types';
-var { CompositeDisposable } = require("atom");
+// import Core from '../core';
+import { replaceSymbol } from './actions';
+var { CompositeDisposable } = require('atom');
 type CompositeDisposable = any;
 declare var atom: any;
 
+
 interface CandidateSymbolsProps extends React.Props<any> {
+    // core: Core,
     candidates: string[]
+    replaceSymbol: (symbol: string) => void
 };
 
 
@@ -16,60 +21,100 @@ const mapStateToProps = (state: View.State) => ({
     candidates: state.inputMethod.candidateSymbols
 })
 
-// const mapDispatchToProps = (dispatch: any) => {
-//     return {
-//     };
-// };
+const mapDispatchToProps = (dispatch: any) => ({
+    replaceSymbol: (symbol: string) => {
+        dispatch(replaceSymbol(symbol))
+    }
+})
 
-class CandidateSymbols extends React.Component<CandidateSymbolsProps, void> {
+
+// the nth candicate
+type Frame = {
+    index: number
+}
+
+
+class CandidateSymbols extends React.Component<CandidateSymbolsProps, Frame> {
+
     private subscriptions: CompositeDisposable;
+
+    constructor() {
+        super();
+        this.state = {
+            index: 0
+        };
+    }
+
     // lifecycle hook, subscribes to Atom's core events here
     componentDidMount() {
-        console.log("mount");
         const commands = {
-            "core:move-up": (event) => {
+            'core:move-up': (event) => {
                 if (!_.isEmpty(this.props.candidates)) {
-                    console.log("up");
-
-                    // this.moveUp();
+                    const newIndex = _.max([
+                        0,
+                        this.state.index - 10
+                    ])
+                    this.setState({ index: newIndex });
                     // this.replaceSymbol(this.selected[0]);
                     event.stopImmediatePropagation();
                 }
             },
-            "core:move-right": (event) => {
+            'core:move-right': (event) => {
                 if (!_.isEmpty(this.props.candidates)) {
-                    console.log("right");
-                    // this.moveRight();
+                    const newIndex = _.min([
+                        this.props.candidates.length - 1,
+                        this.state.index + 1
+                    ])
+                    this.setState({ index: newIndex });
                     // this.replaceSymbol(this.selected[0]);
                     event.stopImmediatePropagation();
                 }
             },
-            "core:move-down": (event) => {
+            'core:move-down': (event) => {
                 if (!_.isEmpty(this.props.candidates)) {
-                    console.log("down");
-                    // this.moveDown();
+                    const newIndex = _.min([
+                        this.props.candidates.length - 1,
+                        this.state.index + 10
+                    ])
+                    this.setState({ index: newIndex });
                     // this.replaceSymbol(this.selected[0]);
                     event.stopImmediatePropagation();
                 }
             },
-            "core:move-left": (event) => {
+            'core:move-left': (event) => {
                 if (!_.isEmpty(this.props.candidates)) {
-                    console.log("left");
-                    // this.moveLeft();
+                    const newIndex = _.max([
+                        0,
+                        this.state.index - 1
+                    ])
+                    this.setState({ index: newIndex });
                     // this.replaceSymbol(this.selected[0]);
                     event.stopImmediatePropagation();
                 }
             }
         }
 
-        this.subscriptions = atom.commands.add("atom-text-editor.agda-mode-input-method-activated", commands);
+        this.subscriptions = atom.commands.add('atom-text-editor.agda-mode-input-method-activated', commands);
     }
 
     render() {
         const { candidates } = this.props;
+        const start = Math.floor(this.state.index / 10) * 10;
+        const position = this.state.index % 10;
+        const frameLeft = candidates.slice(start, this.state.index);
+        const frameRight = candidates.slice(this.state.index + 1, start + 10);
+        const selected = candidates[this.state.index];
         return (
             <div id="candidate-symbols" className="btn-group btn-group-sm">
-                {candidates.map(key => <button
+                {frameLeft.map(key => <button
+                    className="btn"
+                    key={key}
+                >{key}</button>)}
+                <button
+                    className="btn selected"
+                    key={selected}
+                >{selected}</button>
+                {frameRight.map(key => <button
                     className="btn"
                     key={key}
                 >{key}</button>)}
--- src/view/actions.ts
+++ src/view/actions.ts
@@ -3,7 +3,7 @@ import { createAction, handleAction, handleActions, Action } from 'redux-actions
 // import { State, Section, LanguageSection, History } from './types';
 
 
-export type INPUT_METHOD = INPUT_METHOD.ACTIVATE | INPUT_METHOD.DEACTIVATE | INPUT_METHOD.INSERT | INPUT_METHOD.DELETE;
+export type INPUT_METHOD = INPUT_METHOD.ACTIVATE | INPUT_METHOD.DEACTIVATE | INPUT_METHOD.INSERT | INPUT_METHOD.DELETE | INPUT_METHOD.REPLACE_SYMBOL;
 export namespace INPUT_METHOD {
     export const ACTIVATE = 'INPUT_METHOD.ACTIVATE';
     export type ACTIVATE = void;
@@ -13,12 +13,15 @@ export namespace INPUT_METHOD {
     export type INSERT = string;
     export const DELETE = 'INPUT_METHOD.DELETE';
     export type DELETE = void;
+    export const REPLACE_SYMBOL = 'INPUT_METHOD.REPLACE_SYMBOL';
+    export type REPLACE_SYMBOL = string;
 }
 
- export const activateInputMethod = createAction<INPUT_METHOD.ACTIVATE, INPUT_METHOD.ACTIVATE>(INPUT_METHOD.ACTIVATE);
- export const deactivateInputMethod = createAction<INPUT_METHOD.DEACTIVATE, INPUT_METHOD.DEACTIVATE>(INPUT_METHOD.DEACTIVATE);
- export const insertInputMethod = createAction<INPUT_METHOD.INSERT, INPUT_METHOD.INSERT>(INPUT_METHOD.INSERT);
- export const deleteInputMethod = createAction<INPUT_METHOD.DELETE, INPUT_METHOD.DELETE>(INPUT_METHOD.DELETE);
+ export const activateInputMethod = createAction(INPUT_METHOD.ACTIVATE);
+ export const deactivateInputMethod = createAction(INPUT_METHOD.DEACTIVATE);
+ export const insertInputMethod = createAction(INPUT_METHOD.INSERT);
+ export const deleteInputMethod = createAction(INPUT_METHOD.DELETE);
+ export const replaceSymbol = createAction(INPUT_METHOD.REPLACE_SYMBOL);
 
 // export namespace fetch {
 //     export const init = createAction<string, FETCH.INIT>(FETCH.INIT);
