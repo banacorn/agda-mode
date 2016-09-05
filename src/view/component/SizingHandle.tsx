45c87d98095482a3aa7f8df04a45470760951ea3
Ting-Yan Lai <banacorn@gmail.com>
2016-09-05 16:22:22 +0800

    Prevent <ResizingHandle> from occupying space

--- src/view/component/SizingHandle.tsx
+++ src/view/component/SizingHandle.tsx
@@ -18,41 +18,39 @@ interface State {
 class SizingHandle extends React.Component<Props, State> {
     render() {
         const { onResizeStart, onResize, onResizeEnd } = this.props;
-        const style = {
-            cursor: 'row-resize',
-            height: '5px'
-        }
         return (
-            <div
-                onDragStart={(e) => {
-                    this.setState({
-                        initial: e.pageY
-                    })
-                    if (onResizeStart)
-                        onResizeStart(e.pageY);
-                }}
-                onDrag={(e) => {
-                    if (e.pageY !== 0) {    // filter wierd noise out
-                        const offset = e.pageY - this.state.initial;
-                        if (offset !== 0) {
-                            if (onResize)
-                                onResize(offset);
-                            this.setState({
-                                initial: e.pageY
-                            });
+            <div id="agda-sizing-handle-anchor">
+                <div
+                    id="agda-sizing-handle"
+                    onDragStart={(e) => {
+                        this.setState({
+                            initial: e.pageY
+                        })
+                        if (onResizeStart)
+                            onResizeStart(e.pageY);
+                    }}
+                    onDrag={(e) => {
+                        if (e.pageY !== 0) {    // filter wierd noise out
+                            const offset = e.pageY - this.state.initial;
+                            if (offset !== 0) {
+                                if (onResize)
+                                    onResize(offset);
+                                this.setState({
+                                    initial: e.pageY
+                                });
+                            }
                         }
-                    }
-                }}
-                onDragEnd={(e) => {
-                    if (onResizeEnd)
-                        onResizeEnd(e.pageY);
-                }}
-                // to enable Drag & Drop
-                draggable="true"
-                className="native-key-bindings sizing-handle"
-                tabIndex="-1"
-                style={style}
-            ></div>
+                    }}
+                    onDragEnd={(e) => {
+                        if (onResizeEnd)
+                            onResizeEnd(e.pageY);
+                    }}
+                    // to enable Drag & Drop
+                    draggable="true"
+                    className="native-key-bindings sizing-handle"
+                    tabIndex="-1"
+                ></div>
+            </div>
         )
     }
 }
