65bfdadf0495cf76a3c2f894790a2c5d33cda921
Tîng-Giān Luā <banacorn@gmail.com>
2016-09-13 15:46:57 +0800

    Fix #15 regression

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -23,9 +23,9 @@ function parseFilepath(s: string): string {
 function parseInputContent(data: string): string {
     let expr = data.toString()
         .replace(/\\/g, '\\\\')
-        .replace(/\\/g, '\\\\')     // \           => \\
-        .replace(/\"/g, '\"')       // "           => \"
-        .replace(/\n/g, '\\n');     // newline     => \\n
+        .replace(/\\/g, '\\\\')      // \           => \\
+        .replace(/\"/g, '\\"')       // "           => \"
+        .replace(/\n/g, '\\n');      // newline     => \\n
 
     // trim spaces
     if (atom.config.get('agda-mode.trimSpaces'))
