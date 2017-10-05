66f32a4ff380338ea3ee5ed72be8401f8c10847d
Luā "Ian" Tîng-Giān <banacorn@gmail.com>
2017-10-05 23:08:42 +0800

    update legacy internal states when needed

--- src/persist.ts
+++ src/persist.ts
@@ -11,9 +11,44 @@ export type Store = {
     erred: GUID[];
 }
 
+// Example of legacy states before 0.7.2
+const legacyExample = {"connections":[{"guid":"c887b1ba-39e1-4238-be4e-9ed054da2ded","uri":"/home/banacorn/.local/bin/agda","version":{"raw":"2.5.3","sem":"2.5.3"},"protocol":"Vanilla"}],"connected":"c887b1ba-39e1-4238-be4e-9ed054da2ded","erred":["c887b1ba-39e1-4238-be4e-9ed054da2ded"]};
+
+function isLegacyState(state: any): boolean {
+    return _.some(state['connections'], connInfo => {
+        return connInfo['uri'] !== undefined || connInfo['agda'] === undefined;
+    });
+}
+
+function updateLegacyConnectionInfo(connInfo: any): ConnectionInfo {
+    if (connInfo.uri || connInfo.agda === undefined) {
+        connInfo.agda = {
+            location: connInfo.uri,
+            version: connInfo.version
+        };
+        delete connInfo.uri;
+        delete connInfo.version;
+        delete connInfo.protocol;
+        return connInfo;
+    } else {
+        return connInfo;
+    }
+}
+
+function updateLegacyState(state: any): Store {
+    state.connections = state.connections.map(updateLegacyConnectionInfo);
+    return state;
+}
+
 export function get(): Store {
     if (atom.config.get('agda-mode.internalState')) {
-        return JSON.parse(atom.config.get('agda-mode.internalState')) as Store;
+        const state = JSON.parse(atom.config.get('agda-mode.internalState')) as Store;
+
+        if (isLegacyState(state)) {
+            const newState = updateLegacyState(state);
+            set(newState);
+        }
+        return state;
     } else {
         initialize();
         return defaultStore;
